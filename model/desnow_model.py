39116f03f6241c07975b3faf72b6c57c7c2ffdc6
Eren K <90657474+real-eren@users.noreply.github.com>
2022-04-10 19:32:34 -0400

    move to model folder, fix typo in Conv2D call

--- model/desnow_model.py
+++ model/desnow_model.py
@@ -30,7 +30,7 @@ class NoiseReducer(tf.keras.Model):
         self.decoder = tf.keras.Sequential([
           layers.Conv2DTranspose(8, kernel_size=3, strides=2, activation='relu', padding='same'),
           layers.Conv2DTranspose(16, kernel_size=3, strides=2, activation='relu', padding='same'),
-          layers.Conv2D(1, kernel=(3,3), activation='sigmoid', padding='same')])
+          layers.Conv2D(1, kernel_size=(3,3), activation='sigmoid', padding='same')])
 
     def call(self, x):
         encoded = self.encoder(x)
