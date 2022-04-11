92ac68ae538e1328a22c906e732a4291d4de2dbc
real-eren <exk379@case.edu>
2022-04-11 14:40:17 -0400

    working copy of desnow_model. still grayscale

--- model/desnow_model.py
+++ model/desnow_model.py
@@ -1,9 +1,12 @@
+import os
+
 import matplotlib.pyplot as plt
 import tensorflow as tf
 
 from tensorflow.keras import layers, losses
-from tensorflow.keras.datasets import fashion_mnist
 from tensorflow.keras.models import Model
+
+import model_utils as mu
 
 # code based on https://www.tensorflow.org/tutorials/generative/autoencoder
 
@@ -11,14 +14,14 @@ class Denoise(Model):
     def __init__(self):
         super(Denoise, self).__init__()
         self.encoder = tf.keras.Sequential([
-        layers.Input(shape=(28, 28, 1)),
+        layers.Input(shape=(32, 32, 3)),
         layers.Conv2D(16, (3, 3), activation='relu', padding='same', strides=2),
         layers.Conv2D(8, (3, 3), activation='relu', padding='same', strides=2)])
 
         self.decoder = tf.keras.Sequential([
         layers.Conv2DTranspose(8, kernel_size=3, strides=2, activation='relu', padding='same'),
         layers.Conv2DTranspose(16, kernel_size=3, strides=2, activation='relu', padding='same'),
-        layers.Conv2D(1, kernel_size=(3, 3), activation='sigmoid', padding='same')])
+        layers.Conv2D(3, kernel_size=(3, 3), activation='sigmoid', padding='same')])
 
     def call(self, x):
         encoded = self.encoder(x)
@@ -43,17 +46,25 @@ def trained_model(x_train, x_test, x_train_noisy, x_test_noisy, num_epochs=10):
     return autoencoder
 
 
-def plot_input_vs_output(x_test_noisy, decoded_imgs, n):
+def plot_input_vs_output(x_test_clean, x_test_noisy, decoded_imgs, n):
     plt.figure(figsize=(20, 4))
     for i in range(n):
 
-        # display original + noise
+        # display original
         ax = plt.subplot(2, n, i + 1)
-        plt.title("original + noise")
-        plt.imshow(tf.squeeze(x_test_noisy[i]))
+        plt.title("original")
+        plt.imshow(tf.squeeze(x_test_clean[i]))
         plt.gray()
         ax.get_xaxis().set_visible(False)
         ax.get_yaxis().set_visible(False)
+
+        # # display original + noise
+        # ax = plt.subplot(2, n, i + 1)
+        # plt.title("original + noise")
+        # plt.imshow(tf.squeeze(x_test_noisy[i]))
+        # plt.gray()
+        # ax.get_xaxis().set_visible(False)
+        # ax.get_yaxis().set_visible(False)
 
         # display reconstruction
         bx = plt.subplot(2, n, i + n + 1)
@@ -67,30 +78,38 @@ def plot_input_vs_output(x_test_noisy, decoded_imgs, n):
 
 # load snow data, train model, decode sample, display
 if __name__ == "__main__":
-
+    
     # clean
-    (x_train, _), (x_test, _) = fashion_mnist.load_data()
+    clean_imgs, clean_labels = mu.load_corresponding_images(
+        pattern_dir=os.path.join(mu.DATA_DIR, 'ml-examples'),
+        src_dir=os.path.join(mu.DATA_DIR, 'original'),
+        pattern_ext='.png',
+        src_ext='.ppm',
+        )
 
-    x_train = x_train.astype('float32') / 255.
-    x_test = x_test.astype('float32') / 255.
+    num_train = int(0.8 * len(clean_imgs))
 
-    x_train = x_train[..., tf.newaxis]
-    x_test = x_test[..., tf.newaxis]
-
+    clean_img_array = mu.wrap_np_array(clean_imgs)[..., tf.newaxis]
+    clean_train = clean_img_array[:num_train] # first n
+    clean_test = clean_img_array[num_train:] # remainder
+    
+    print(clean_img_array.shape)
+    print(clean_test.shape)
     # noisy
-    noise_factor = 0.2
-    x_train_noisy = x_train + noise_factor * tf.random.normal(shape=x_train.shape) 
-    x_test_noisy = x_test + noise_factor * tf.random.normal(shape=x_test.shape) 
-
-    x_train_noisy = tf.clip_by_value(x_train_noisy, clip_value_min=0., clip_value_max=1.)
-    x_test_noisy = tf.clip_by_value(x_test_noisy, clip_value_min=0., clip_value_max=1.)
+    noisy_imgs, noisy_labels = mu.load_images(
+        img_dir=os.path.join(mu.DATA_DIR, 'ml-examples'),
+        img_ext='.png',
+        )
+    noisy_img_array = mu.wrap_np_array(noisy_imgs)[..., tf.newaxis]
+    noisy_train = noisy_img_array[:num_train] # first n
+    noisy_test = noisy_img_array[num_train:] # remainder
 
     # train model on data
-    denoiser_model = trained_model(x_train, x_test, x_train_noisy, x_test_noisy, 3)
+    denoiser_model = trained_model(clean_train, clean_test, noisy_train, noisy_test, num_epochs=5)
 
     # use model to denoise noisy images
-    decoded_imgs = denoiser_model(x_test_noisy)
+    decoded_imgs = denoiser_model(noisy_test)
 
     # plot them
-    plot_input_vs_output(x_test_noisy, decoded_imgs, n=10)
+    plot_input_vs_output(clean_test, noisy_test, decoded_imgs, n=10)
 
