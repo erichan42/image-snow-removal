e943cdc02eba3c4361f27f19185868f004142442
real-eren <exk379@case.edu>
2022-04-11 17:26:32 -0400

    move main script into `if main` block

--- main.py
+++ main.py
@@ -3,14 +3,15 @@ import argparse
 
 import scripts.dataparser as dp
 
-ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+if __name__ == "__main__":
+    ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 
-parser = argparse.ArgumentParser(description='Image processing using GPUs')
-parser.add_argument('--init-data', '-i', action='store_true')
-parser.add_argument('--force', '-f', action='store_true')
-parser.add_argument('--set-top', '-t', type=int, default=10)
-parser.add_argument('--set-threshold', type=int, default=50*50)
-args = parser.parse_args()
+    parser = argparse.ArgumentParser(description='Image processing using GPUs')
+    parser.add_argument('--init-data', '-i', action='store_true')
+    parser.add_argument('--force', '-f', action='store_true')
+    parser.add_argument('--set-top', '-t', type=int, default=10)
+    parser.add_argument('--set-threshold', type=int, default=50*50)
+    args = parser.parse_args()
 
-if args.init_data:
-    dp.store_data(f'{ROOT_DIR}/data', args.set_top, args.set_threshold, args.force)
+    if args.init_data:
+        dp.store_data(f'{ROOT_DIR}/data', args.set_top, args.set_threshold, args.force)
