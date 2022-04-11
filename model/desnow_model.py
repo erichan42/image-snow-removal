81cc7c8da4c152b8b418242c20a9a2736806b845
real-eren <exk379@case.edu>
2022-04-11 12:42:38 -0400

    rewrite desnow model

--- model/desnow_model.py
+++ model/desnow_model.py
@@ -1,76 +1,96 @@
-from importlib.metadata import PathDistribution
-import os
-from pickletools import optimize
-
-from  matplotlib import pyplot as plt
-import matplotlib.image as mpimg
-import numpy as np
-import random
-
-import cv2
+import matplotlib.pyplot as plt
 import tensorflow as tf
-from tensorflow.keras import datasets, layers, models
 
-from model import create_dataset, load_dataset
+from tensorflow.keras import layers, losses
+from tensorflow.keras.datasets import fashion_mnist
+from tensorflow.keras.models import Model
 
-# Code based on `https://towardsdatascience.com/image-noise-reduction-in-10-minutes-with-convolutional-autoencoders-d16219d2956a`
+# code based on https://www.tensorflow.org/tutorials/generative/autoencoder
 
-__imageX = 32
-__imageY = 32
-
-class NoiseReducer(tf.keras.Model):
-    def __init__(self) -> None:
-        super(NoiseReducer, self).__init__()
-
+class Denoise(Model):
+    def __init__(self):
+        super(Denoise, self).__init__()
         self.encoder = tf.keras.Sequential([
-          layers.Input(shape=(__imageX, __imageY, 1)),
-          layers.Conv2D(16, (3,3), activation='relu', padding='same', strides=2),
-          layers.Conv2D(8, (3,3), activation='relu', padding='same', strides=2)])
+        layers.Input(shape=(28, 28, 1)),
+        layers.Conv2D(16, (3, 3), activation='relu', padding='same', strides=2),
+        layers.Conv2D(8, (3, 3), activation='relu', padding='same', strides=2)])
 
         self.decoder = tf.keras.Sequential([
-          layers.Conv2DTranspose(8, kernel_size=3, strides=2, activation='relu', padding='same'),
-          layers.Conv2DTranspose(16, kernel_size=3, strides=2, activation='relu', padding='same'),
-          layers.Conv2D(1, kernel_size=(3,3), activation='sigmoid', padding='same')])
+        layers.Conv2DTranspose(8, kernel_size=3, strides=2, activation='relu', padding='same'),
+        layers.Conv2DTranspose(16, kernel_size=3, strides=2, activation='relu', padding='same'),
+        layers.Conv2D(1, kernel_size=(3, 3), activation='sigmoid', padding='same')])
 
     def call(self, x):
         encoded = self.encoder(x)
         decoded = self.decoder(encoded)
         return decoded
 
-# returns a trained model
-def train_model(clean_train, noisy_train, clean_test, noisy_test):
-  autoencoder = NoiseReducer()
-
-  autoencoder.compile(optimizer='adam', loss='mse')
-  autoencoder.fit(noisy_train,
-                  clean_train,
-                  epochs=10,
-                  shuffle=True,
-                  validation_data=(noisy_test, clean_test))
-
-  return autoencoder
+    def print_summaries(self):
+        self.encoder.summary()
+        self.decoder.summary()
 
 
-def load_dataset_and_train_model():
-  noisy_img_data, class_names = load_dataset(trainSnow=True)
-  clean_img_data, class_names = load_dataset(trainSnow=False)
+def trained_model(x_train, x_test, x_train_noisy, x_test_noisy, num_epochs=10):
+    """Returns an autoencoder trained on the given data"""
+    autoencoder = Denoise()
 
-  num_noisy_total = len(noisy_img_data)
-  num_noisy_train = 4 * num_noisy_total // 5
+    autoencoder.compile(optimizer='adam', loss=losses.MeanSquaredError())
 
-  num_clean_total = len(clean_img_data)
-  num_clean_train = 4 * num_clean_total // 5
+    autoencoder.fit(x_train_noisy, x_train,
+                    epochs=num_epochs,
+                    shuffle=True,
+                    validation_data=(x_test_noisy, x_test))
+    return autoencoder
 
-  model = train_model(
-    clean_img_data[:num_clean_train],
-    noisy_img_data[:num_noisy_train],
-    clean_test= clean_img_data[num_clean_train:],
-    noisy_test= noisy_img_data[num_noisy_train:])
 
+def plot_input_vs_output(x_test_noisy, decoded_imgs, n):
+    plt.figure(figsize=(20, 4))
+    for i in range(n):
+
+        # display original + noise
+        ax = plt.subplot(2, n, i + 1)
+        plt.title("original + noise")
+        plt.imshow(tf.squeeze(x_test_noisy[i]))
+        plt.gray()
+        ax.get_xaxis().set_visible(False)
+        ax.get_yaxis().set_visible(False)
+
+        # display reconstruction
+        bx = plt.subplot(2, n, i + n + 1)
+        plt.title("reconstructed")
+        plt.imshow(tf.squeeze(decoded_imgs[i]))
+        plt.gray()
+        bx.get_xaxis().set_visible(False)
+        bx.get_yaxis().set_visible(False)
+    plt.show()
+
+
+# load snow data, train model, decode sample, display
 if __name__ == "__main__":
-  
-  # imgg = cv2.imread(os.path.join(os.path.dirname(__file__), 'testimage.ppm'), cv2.COLOR_BGR2RGB)
-  # cv2.imshow('window', imgg)
-  # cv2.waitKey(5000)
-  # cv2.imwrite('testimage.png', imgg)
-  load_dataset_and_train_model()
+
+    # clean
+    (x_train, _), (x_test, _) = fashion_mnist.load_data()
+
+    x_train = x_train.astype('float32') / 255.
+    x_test = x_test.astype('float32') / 255.
+
+    x_train = x_train[..., tf.newaxis]
+    x_test = x_test[..., tf.newaxis]
+
+    # noisy
+    noise_factor = 0.2
+    x_train_noisy = x_train + noise_factor * tf.random.normal(shape=x_train.shape) 
+    x_test_noisy = x_test + noise_factor * tf.random.normal(shape=x_test.shape) 
+
+    x_train_noisy = tf.clip_by_value(x_train_noisy, clip_value_min=0., clip_value_max=1.)
+    x_test_noisy = tf.clip_by_value(x_test_noisy, clip_value_min=0., clip_value_max=1.)
+
+    # train model on data
+    denoiser_model = trained_model(x_train, x_test, x_train_noisy, x_test_noisy, 3)
+
+    # use model to denoise noisy images
+    decoded_imgs = denoiser_model(x_test_noisy)
+
+    # plot them
+    plot_input_vs_output(x_test_noisy, decoded_imgs, n=10)
+
