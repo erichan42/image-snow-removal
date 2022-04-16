083d99a1a45c9a6b24bd71678b0e16fd32135e15
Eren K <90657474+real-eren@users.noreply.github.com>
2022-04-16 18:47:12 -0400

    remove dead code

--- scripts/noise.py
+++ scripts/noise.py
@@ -1,5 +1,4 @@
 import cv2
-import os # don't need
 
 
 # scales mask down to dimensions
@@ -17,59 +16,3 @@ def apply(img_filename, mask_filenames, width, height):
         img = cv2.add(img, mask)
 
     return img
-
-
-# DONT NEED THIS
-def apply_and_store(img_filename, mask_filename, destfilename, log_fun = print):
-    """
-    composites images from two files, writes result to the destfilename
-
-    @param img_filename  
-    @param log_fun  1-arg function that is invoked on a filename if cv2::imread fails while opening it
-    """
-    try:
-        fg_img = cv2.imread(img_filename)
-    except:
-        log_fun(img_filename)
-        return
-    try:
-        mask_img = cv2.imread(mask_filename)
-    except:
-        log_fun(mask_filename)
-        return
-
-    masked_img = cv2.add(fg_img, masked_img)
-    try:
-        cv2.imwrite(destfilename, masked_img)
-    except:
-        log_fun(destfilename)
-        return
-
-
-# DONT NEED THIS
-def apply_over_folders(mask_folder, img_folder, metadata_file, dest_folder):
-    """ 
-    for each file in img_folder, applies a random mask and writes to dest_folder
-    """
-    img_dimensions = {}
-    # get dimensions from metadata file (csv) and store into dictionary
-        # read all lines
-    with open(metadata_file) as metadata:
-        for row in metadata:
-            # Filename;Width;Height;Roi.X1;Roi.Y1;Roi.X2;Roi.Y2;ClassId
-            # split by ';'
-            d = { i :[] for i in range(0, 50) }
-            name, width, height, _ = row.split(';')
-            # map filename to dimension tuple
-            img_dimensions[name] = ()
-    
-    # iterate over the img_folder (class folder) directory
-        # directory path
-        # for file in os.listdir(img_folder):
-
-    # pick a random mask
-
-    # apply mask
-
-    # write to dest folder
-    
